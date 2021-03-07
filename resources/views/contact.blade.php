72374686d07f8bda53e5948b06f579a6eb2dddce
f-sajib <faisalsajib1994@gmail.com>
2021-03-07 16:26:28 +0600

    set up docker for phpmyadmin

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -22,8 +22,14 @@
 </head>
 <body>
 	@if (session('message') && session('alert'))
-		<div class="alert alert-{{ session('alert') }}">
-			{{ session('message') }}
+		<div class="alert alert-dismissable alert-{{ session('alert') }}">
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+		<strong>
+		   {{ session('message') }}
+		</strong>
+			
 		</div>
 	@endif
 	<div class="contact1">
