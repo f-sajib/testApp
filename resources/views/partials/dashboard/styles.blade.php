943e7dbd7e60be5f81b5cf412402619bb99a90bb
f-sajib <faisalsajib1994@gmail.com>
2021-07-23 11:05:39 +0600

    completed auth scenrio

---
+++ resources/views/partials/dashboard/styles.blade.php
@@ -0,0 +1,7 @@
+<link href="{{asset('admin/css/material-dashboard.css?v=2.1.2')}}" rel="stylesheet" />
+<link href="{{asset('admin/demo/demo.css')}}" rel="stylesheet" />
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+{{--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">--}}
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" />
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="{{asset('admin/js/core/jquery.min.js')}}"></script>
